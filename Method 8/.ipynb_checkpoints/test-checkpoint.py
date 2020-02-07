dcf7ce7e8e926760675a2c7bf94c0fe26607615a
Hector Daniel Moreno Leyva <hector2_10@hotmail.com>
2020-02-07 09:43:22 -0600

    change test function

--- Method 8/.ipynb_checkpoints/test-checkpoint.py
+++ Method 8/.ipynb_checkpoints/test-checkpoint.py
@@ -42,14 +42,16 @@ class Testing1():
         totalMatchStatic = 0
         totalStatic = 0
         totalDynamic = 0
+        accuracy = 0
         for cell0,cell1,cell2 in zip(row_prev,row,row_pred):
             totalMatchDynamic += (cell0 != cell1 and cell1 == cell2)
             totalMatchStatic += (cell0 == cell1 and cell1 == cell2)
             totalStatic += (cell0 == cell1)
             totalDynamic += (cell0 != cell1)
+            accuracy += (cell1 == cell2)
         totalStatic = totalStatic if totalStatic > 0 else 0
         totalDynamic = totalDynamic if totalDynamic > 0 else 0
-        return [totalStatic,totalDynamic,totalMatchStatic,totalMatchDynamic]
+        return [totalStatic,totalDynamic,totalMatchStatic,totalMatchDynamic,accuracy]
             
     def run(self):
         res = []
@@ -61,17 +63,24 @@ class Testing1():
                 prediction = next(self.model.run(self.data[i-1],1))
                 rowsTotal = parallel(delayed(self.testRow)(row0,row1,row2) for row0,row1,row2 in zip(self.data[i-1],self.data[i],prediction[1]))
                 summation = np.sum(rowsTotal,axis=0)
-                if self.verbose >= 1: 
+                if self.verbose >= 2: 
                     print(f"Static : {summation[2]}/{summation[0]}")
                     print(f"Dynamic : {summation[3]}/{summation[1]}")
+                    print(f"Accuracy: {summation[4]} / {self.data[i].size}")
                 totalStatic = summation[2]/summation[0] if summation[0] > 0 else 0
                 totalDynamic = summation[3]/summation[1] if summation[1] > 0 else 0
-                res.append([totalStatic,totalDynamic])
+                accuracy = summation[4] / self.data[i].size
+                res.append([totalStatic,totalDynamic,accuracy])
         self.result = np.array(res)
     
     def plot(self,figsize=(15,10)):
         plt.figure(figsize=figsize)
-        plt.plot(range(len(self.result)),self.result[:,0],label="Static")
-        plt.plot(range(len(self.result)),self.result[:,1],label="Dynamic")
+        plt.plot(range(len(self.result)),self.result[:,0],label="Trivial (static behaviour)")
+        plt.plot(range(len(self.result)),self.result[:,1],label="Not trivial (Not static behaviour)")
+        plt.plot(range(len(self.result)),self.result[:,2],label="Accuracy (General behaviour)")
+        plt.yticks(np.arange(0, 1, step=0.05))
+        plt.ylabel("Accuracy")
+        plt.xlabel("Simulation step")
+        plt.grid(True)
         plt.legend()
         return plt
